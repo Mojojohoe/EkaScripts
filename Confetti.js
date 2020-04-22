226abbad6c2d7554a4f23a47553d7df7d189e264
Jobix <obixwrepper@gmail.com>
2020-04-22 21:53:22 +0100

    Update Confetti.js

--- Confetti.js
+++ Confetti.js
@@ -1,47 +1,4 @@
-	$(function() {
-		
-	$('.pb').each(function(){
-		var t = $(this);
-		var dataperc = t.attr('data-perc'),
-				barperc = Math.round(dataperc*5.56/2);
-		t.find('.bar').animate({width:barperc}, dataperc*0);
-		t.find('.label').append('<div class="perc"></div>');
-		function perc() {
-			var length = t.find('.bar').css('width'),
-				perc = Math.round(parseInt(length)/5.56),
-				labelpos = (parseInt(length)-2);
-			t.find('.label').css('left', labelpos);
-			t.find('.perc').text(perc+'%');
-		}
-		perc();
-		setInterval(perc, 0); 
-	});
-});
-$(function() {
-		
-	$('.pb').each(function(){
-		var t = $(this);
-		var dataperc = t.attr('data-perc'),
-				barperc = Math.round(dataperc*5.56/2);
-				leftamount = (100 - barperc) +204;
-		t.find('.barneg').animate({width:barperc, left: leftamount}, dataperc*0);
-		t.find('.label').append('<div class="perc"></div>');
-		
-		function perc() {
-			var length = t.find('.bar').css('width'),
-				perc = Math.round(parseInt(length)/5.56),
-				labelpos = (parseInt(length)-2);
-			t.find('.label').css('left', labelpos);
-			t.find('.perc').text(perc+'%');
-		}
-		perc();
-		setInterval(perc, 0); 
-	});
-	
-	});
 
-</script>
-<script>
 $.fn.extend({
   'progressbar': function(config) {
     var opt = $.extend({
