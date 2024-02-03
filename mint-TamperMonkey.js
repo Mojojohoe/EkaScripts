9bdeefef70a2eb12366f117a0abb175d7930f717
Mojojohoe <56446579+Mojojohoe@users.noreply.github.com>
2024-02-03 20:54:50 +0000

    attempt 2

--- mint-TamperMonkey.js
+++ mint-TamperMonkey.js
@@ -2,7 +2,7 @@
 // @name         Eka's Chat Mint
 // @namespace    http://tampermonkey.net/
 // @homepage     https://z0r.de/7432
-// @version      0.1.58
+// @version      0.1.59
 // @icon         https://rp.aryion.com/img/profile/184938_f0842d7490194c2b9574ba049f3dda06.png
 // @description  Alpha version "Melting-mint-choc" (mods in new things)
 // @author       Jobix
@@ -1413,7 +1413,7 @@ if (
           document.addEventListener('click', function(event) {
             var clickedElement = event.target;
             var idREF = parseInt(clickedElement.id.split('_')[1]);
-            var parentUl = clickedElement.closest('ul'); // Get the closest <ul> ancestor
+            var parentUl = clickedElement.closest('li'); 
         
             if (clickedElement.classList.contains('remove-from-bin')) {
                 clickedElement.style.display = "none";
