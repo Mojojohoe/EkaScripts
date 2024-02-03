919fb34cd447cbb3ca0f4a31537b8527d838bf30
Mojojohoe <56446579+Mojojohoe@users.noreply.github.com>
2024-02-03 18:04:42 +0000

    Version update

--- mint-TamperMonkey.js
+++ mint-TamperMonkey.js
@@ -2,7 +2,7 @@
 // @name         Eka's Chat Mint
 // @namespace    http://tampermonkey.net/
 // @homepage     https://z0r.de/7432
-// @version      0.1.45
+// @version      0.1.46
 // @description  mods in new things
 // @author       Jobix
 // @match        https://rp.aryion.com/*
